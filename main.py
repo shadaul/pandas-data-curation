d2d30a2185be7e051bf45399d0a324c38a7d6e3a
daulet <shabakovdaulet@gmail.com>
2026-04-07 13:02:21 +0200

    implement 10-hour background scheduler for ETL pipeline and add gitignore with requirements

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import subprocess
+import time
 
-print("calling to api")
-subprocess.run(["python", "api_test.py"])
 
-print("cleaning and adding to db")
-subprocess.run(["python", "scripts.py"])
+while True:
+    print("calling to api")
+    subprocess.run(["python", "api_test.py"])
 
+    print("cleaning and adding to db")
+    subprocess.run(["python", "scripts.py"])
+
+    print("waiting 10 hours before running it again")
+    time.sleep(36000)
 
 # subprocess.run(["python","check_db.py"])
 # print("checking database")
