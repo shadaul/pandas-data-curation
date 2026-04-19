7237ed99279776bae330898ce9f3954a12dc00fa
daulet <shabakovdaulet@gmail.com>
2026-04-19 03:00:06 +0200

    sql python functions to practice with db

--- test/test_dict/dict.py
+++ test/test_dict/dict.py
@@ -23,20 +23,108 @@
 # print(get_top_spenders(transactions, 2))
 
 
-sessions = [
-    {"user_id": 1, "duration": 45},
-    {"user_id": 2, "duration": 12},
-    {"user_id": 1, "duration": 80},
-    {"user_id": 3, "duration": 25},
-    {"user_id": 2, "duration": 30}
+# sessions = [
+#     {"user_id": 1, "duration": 45},
+#     {"user_id": 2, "duration": 12},
+#     {"user_id": 1, "duration": 80},
+#     {"user_id": 3, "duration": 25},
+#     {"user_id": 2, "duration": 30}
+# ]
+
+# def get_max_session(logs):
+#     my_dict = {}
+#     for item in logs:
+#         user = item["user_id"]
+#         time = item["duration"]
+#         if user not in my_dict:
+#             my_dict[user] = time
+#         else:
+#             if time > my_dict[user]:
+#                 my_dict[user] = time
+#     return my_dict
+
+# print(get_max_session(sessions))
+
+# server_logs = [
+#     {"status_code": 200, "message": "OK"},
+#     {"status_code": 404, "message": "Not Found"},
+#     {"status_code": 500, "message": "Internal Error"},
+#     {"status_code": 200, "message": "OK"},
+#     {"status_code": 404, "message": "Not Found"},
+#     {"status_code": 403, "message": "Forbidden"},
+#     {"status_code": 404, "message": "Not Found"}
+# ]
+
+# def count_errors(logs):
+#     my_dict = {}
+#     for item in logs:
+#         status = item["status_code"]
+#         mess = item["message"]
+#         if status != 200:
+#             my_dict[status] = my_dict.get(status, 0) + 1
+
+#     return my_dict
+
+# print(count_errors(server_logs))
+
+# users = [
+#     {"id": 1, "name": "Али"},
+#     {"id": 2, "name": "Борис"},
+#     {"id": 3, "name": "Витя"}
+# ]
+
+# purchases = [
+#     {"user_id": 1, "amount": 500},
+#     {"user_id": 2, "amount": 200},
+#     {"user_id": 1, "amount": 100},
+#     # Обрати внимание, Витя ничего не купил!
+# ]
+
+# def get_total_spent(users, purchases):
+#     my_dict = {}
+    
+#     final_dict = {}
+
+
+#     for person in users:
+#         user_id = person["id"]
+#         name = person["name"]
+#         my_dict[user_id] = name
+
+#         final_dict[name] = 0
+#         print(my_dict)
+
+#     for item in purchases:
+#         u_id = item["user_id"]
+#         money = item["amount"]
+
+#         real_name = my_dict[u_id]
+
+#         final_dict[real_name] = final_dict.get(real_name) + money
+
+#     return final_dict
+
+# print(get_total_spent(users, purchases))
+
+clicks = [
+    {"user_id": 1, "page": "Главная"},
+    {"user_id": 2, "page": "Каталог"},
+    {"user_id": 1, "page": "Корзина"},
+    {"user_id": 3, "page": "Главная"},
+    {"user_id": 2, "page": "Оплата"}
 ]
 
-def get_max_session(logs):
+def group_clicks(logs):
     my_dict = {}
     for item in logs:
-        user = item["user_id"]
-        time = item["duration"]
-        my_dict[user] = max(time, my_dict.get(user, 0))
+        id = item["user_id"]
+        page = item["page"]
+
+        if id not in my_dict:
+            my_dict[id] = [page]
+        else:
+            my_dict[id].append(page)
     return my_dict
 
-print(get_max_session(sessions))
+
+print(group_clicks(clicks))
--- test/test_dict/dict.sql
+++ test/test_dict/dict.sql
@@ -16,5 +16,36 @@
 -- FROM RankedLogs
 -- WHERE row_num = 1
 
-SELECT order_id, status, ROW_NUMBER() OVER (PARTITION BY order_id ORDER BY log_time DESC) as sticker_num 
-FROM order_logs;
+-- SELECT order_id, status, ROW_NUMBER() OVER (PARTITION BY order_id ORDER BY log_time DESC) as sticker_num 
+-- FROM order_logs;
+
+-- WITH mytable as(
+--     SELECT
+--         item_name,
+--         purchase_time,
+--         client_id,
+--         ROW_NUMBER() OVER(
+--             PARTITION by client_id ORDER by purchase_time ASC
+--         ) as row_num
+--     FROM sales
+-- )
+-- SELECT
+--     client_id,
+--     item_name
+-- FROM mytable
+-- WHERE row_num = 1
+
+WITH time_machine AS (
+
+    SELECT 
+        client_id, 
+        visit_date, 
+        LAG(visit_date) OVER (PARTITION by client_id ORDER by visit_date ASC) as prev_visit
+    FROM visits
+)
+
+SELECT 
+    client_id,
+    visit_date,
+    DATEDIFF(day, prev_visit, visit_date) as days_since_last
+FROM time_machine;
